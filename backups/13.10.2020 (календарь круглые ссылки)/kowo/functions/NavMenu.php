405f9710a1ac857f354cfd4e1329d0bd9ac5f22e
dmitrishinbv <dmitrishinbv@gmail.com>
2020-10-29 15:24:48 +0200

    Full release

--- backups/13.10.2020 (календарь круглые ссылки)/kowo/functions/NavMenu.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*register main menu*/
-add_action( 'after_setup_theme', 'ThemeRegisterNavMenu' );
-function ThemeRegisterNavMenu() {
-    register_nav_menu( 'main_nav_menu', 'Main menu');
-}
-?>
--- backups/13.10.2020 (календарь круглые ссылки)/kowo/functions/available_places/AvailablePlacesMain.php
+++
@@ -1,5 +0,0 @@
-<?php
-get_template_part('functions/available_places/AddPostType');
-get_template_part('functions/available_places/AddMetaBox');
-get_template_part('functions/available_places/AddGallery');
-?>
